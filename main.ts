0c0884a03b511b51e2b4758fa00cd0f4abec8916
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-10-12 21:12:35 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack Eddy
+ * Created on: Oct 2023
+ * This program activates Neopixels
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+    input.onButtonPressed(Button.A, function () {
+        let stripGreen = neopixel.create(DigitalPin.P16, 12, NeoPixelMode.RGB)
+        stripGreen.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(1000)
+        stripGreen.showColor(neopixel.colors(NeoPixelColors.Black))
+        let stripRed = neopixel.create(DigitalPin.P16, 12, NeoPixelMode.RGB)
+        stripRed.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(1000)
+        stripRed.showColor(neopixel.colors(NeoPixelColors.Black))
+        let stripYellow = neopixel.create(DigitalPin.P16, 12, NeoPixelMode.RGB)
+        stripYellow.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        basic.pause(1000)
+        stripYellow.showColor(neopixel.colors(NeoPixelColors.Black))
+    })
