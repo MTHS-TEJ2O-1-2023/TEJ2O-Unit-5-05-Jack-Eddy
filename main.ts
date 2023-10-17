d62c22c1e72ad76fb63a741a49804c92ebcc9fe7
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-10-17 12:35:46 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -17,17 +17,24 @@ neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
+
+// start lights  
 input.onButtonPressed(Button.A, function () {
+  // set green light
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.show()
   basic.pause(1000)
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
   neopixelStrip.show()
+
+  // set yellow light
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
   neopixelStrip.show()
   basic.pause(1000)
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
   neopixelStrip.show()
+
+ // set red light
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
   neopixelStrip.show()
   basic.pause(1000)
